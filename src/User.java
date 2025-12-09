92b155425f5377b5645585090d3d90710dbe7235
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-12-09 07:38:38 +0330

    change User

--- src/User.java
+++ src/User.java
@@ -1,6 +1,21 @@
-public class User implements Cloneable {
+public class User implements Cloneable, TextCalculator {
 
-    public static <T,V> void print(T key, V value) {
+    public static <T, V> void print(T key, V value) {
         System.out.println(key + " = " + value);
+    }
+
+    @Override
+    public double calculate(String text) {
+        return 0;
+    }
+
+    @Override
+    public double calculate(String text, int num) {
+        return 0;
+    }
+
+    @Override
+    public double calculate(String text, double num) {
+        return 0;
     }
 }
